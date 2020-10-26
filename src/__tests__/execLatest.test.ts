0eb383bab3cf776088b18445e7acdd6f3e32d43a
alexandrehpiva <alexandrehpiva@gmail.com>
2020-10-25 23:08:49 -0300

    Improved test to prove that really is the lastest call

--- src/__tests__/execLatest.test.ts
+++ src/__tests__/execLatest.test.ts
@@ -1,23 +1,34 @@
 import execLatest from '../execLatest'
 import timeoutLoop from 'timeout-loop'
 
+type Exec = {
+  loopCounter: number,
+  execCounter: number
+}
+
 describe('Execute only the latest call received in a determined time in milliseconds', () => {
   it('Should execute only the latest call in 3 calls made in 100 milliseconds each', async () => {
     let loopCounter = 0
     let execCounter = 0
+    const execTrace: Exec[] = []
 
     await new Promise(async resolveExecLatest => {
       await timeoutLoop(() => {
+        loopCounter += 1
+
         execLatest(() => {
           execCounter += 1
+
+          execTrace.push({ loopCounter, execCounter })
           resolveExecLatest()
         })
-
-        loopCounter += 1
       }, 100, 3)
     })
 
+    // Three executions
     expect(loopCounter).toEqual(3)
-    expect(execCounter).toEqual(1)
+
+    // Only the latest call stored in execTrace
+    expect(execTrace).toEqual([{ loopCounter: 3, execCounter: 1 }])
   })
 })
